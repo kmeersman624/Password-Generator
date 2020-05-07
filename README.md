db15118f107465c92723e00dea5fc1b5fb400926
kmeersman <katie4682@yahoo.com>
2020-05-07 16:31:40 -0600

    Added finishing touches to passwaord generator

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 # Password-Generator
 
 ## Description
+
 This application generates a random password based on user-selected criteria. This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code. It also features a clean and polished user interface and is responsive, ensuring that it adapts to multiple screen sizes.
 
 ## Usage
-CLick the button and answer the prompts to create a unique password that includes the following criteria:
-- At least 8 characters
-- Lowercase letter
-- Uppercase letter
-- Number
-- Special character
 
-## Badges
+CLick the button and answer the prompts to create a unique password that includes the following criteria:
+
+- At least 8 characters, but less than 128
+
+  Contains at least one of the following types of characters:
+  - Lowercase letter
+  - Uppercase letter
+  - Number
+  - Special character
 
 ## License
-© 2020 Kate Meersman brand. All Rights Reserved.
 
+© 2020 Kate Meersman brand. All Rights Reserved.
--- script.js
+++ script.js
@@ -84,141 +84,80 @@ var upperCasedCharacters = [
   "Y",
   "Z",
 ];
-
-// Assignment Code
+//declare generate button to refer back to html generate id
 var generateBtn = document.querySelector("#generate");
 
-//Write password to the #password input
+//Starter code
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+}
 
-  function generatePassword() {
-    //Prompt user for pasword
-    var length = prompt("How long would you like password to be?");
+function generatePassword() {
+  //Prompt user for pasword
+  var length = prompt("How long would you like password to be?");
 
-    length = parseInt(length);
+  length = parseInt(length);
 
-    //If not a number, tell user
-    if (isNaN(length)) {
-      alert("That is not a valid number, please try again");
-      return;
-      ("Not a valid number");
+  //If not a number, tell user
+  if (isNaN(length)) {
+    alert("That is not a valid number, please try again");
+    return;
+    ("Not a valid number");
+  }
+  //If not long enough, tell user
+  if (length < 8) {
+    alert("Your password is too short");
+    return;
+  }
+  //If too long, tell user
+  if (length > 128) {
+    alert("Your password is too long");
+    return;
+  }
+  //Check criteria that user wants to include in password.  Must include at least 1 of the criteria
+  var specialChars = confirm(
+    "Click OK to confirm including special characters"
+  );
+  var numericChars = confirm(
+    "Click OK to confirm including numeric characters"
+  );
+  var lowercaseChars = confirm(
+    "Click OK to confirm including lowercase characters"
+  );
+  var uppercaseChars = confirm(
+    "Click OK to confirm including uppercase characters"
+  );
+  //If user picks no elements
+  if (!specialChars && !numericChars && !lowercaseChars && !uppercaseChars) {
+    alert("You need to select at least one!");
+    return;
+  }
+
+  var password = "";
+
+  //Create for loop to get random password from character arrays
+  for (i = 0; i < length; i++) {
+    if (specialChars) {
+      password += getRandomValue(specialCharacters);
     }
-    //If not long enough, tell user
-    if (length < 8) {
-      alert("Your password is too short");
-      return;
+    if (lowercaseChars) {
+      password += getRandomValue(lowerCasedCharacters);
     }
-    //If too long, tell user
-    if (length > 128) {
-      alert("Your password is too long");
-      return;
+    if (uppercaseChars) {
+      password += getRandomValue(upperCasedCharacters);
     }
-    //Check criteria that user wants to include in password.  Must include at least 1 of the criteria
-    var specialChars = confirm(
-      "Click OK to confirm including special characters"
-    );
-    var numericChars = confirm(
-      "Click OK to confirm including numeric characters"
-    );
-    var lowercaseChars = confirm(
-      "Click OK to confirm including lowercase characters"
-    );
-    var uppercaseChars = confirm(
-      "Click OK to confirm including uppercase characters"
-    );
-    //If user picks no elements
-    if (!specialChars && !numericChars && !lowercaseChars && !uppercaseChars) {
-      alert("You need to select at least one!");
-      return;
+    if (numericChars) {
+      password += getRandomValue(numericCharacters);
     }
   }
-  //If user pick special, numeric, and lower case
-  if (specialChars && numericChars && lowercaseChars) {
-    password = character.concat(
-      specialCharacters,
-      numericCharacters,
-      lowerCasedCharacters
-    );
-  }
-  //If user pick special, numeric, and upper
-  else if (specialChars && numericChars && uppercaseChars) {
-    password = character.concat(
-      specialCharacters,
-      numericCharacters,
-      upperCasedCharacters
-    );
-  }
-  //If user pick special, lower, and upper
-  else if (specialChars && lowercaseChars && uppercaseChars) {
-    password = character.concat(
-      specialCharacters,
-      upperCasedCharacters,
-      lowerCasedCharacters
-    );
-  }
-  //If user pick lower, numeric, and upper
-  else if (uppercaseChars && numericChars && lowercaseChars) {
-    password = character.concat(
-      numericCharacters,
-      upperCasedCharacters,
-      lowerCasedCharacters
-    );
-  }
-  //If user picks upper and lower
-  else if (uppercaseChars && lowercaseChars) {
-    password = character.concat(upperCasedCharacters, lowerCasedCharacters);
-  }
-  //if user picks lower and numeric
-  else if (numericChars && lowercaseChars) {
-    password = character.concat(numericCharacters, lowerCasedCharacters);
-  }
-  //if user picks upper and mumeric
-  else if (numericChars && uppercaseChars) {
-    password = character.concat(numericCharacters, upperCasedCharacters);
-  }
-  //If user picks special and upper
-  else if (specialChars && uppercaseChars) {
-    password = character.concat(specialCharacters, upperCasedCharacters);
-  }
-  //If user picks special and lower
-  else if (specialChars && lowercaseChars) {
-    password = character.concat(specialCharacters, lowerCasedCharacters);
-  }
-  //If user picks special and numeric
-  else if (specialChars && numericChars) {
-    password = character.concat(specialCharacters, numericCharacters);
-  }
-  //If user picks numeric only
-  else if (numericChars) {
-    password = numericCharacters;
-  }
-  //if user picks upper only
-  else if (uppercaseChars) {
-    password = upperCasedCharacters;
-  }
-  //if user picks lower only
-  else if (lowercaseChars) {
-    password = lowerCasedCharacters;
-  }
-  //if user picks special only
-  else if (specialChars) {
-    password = specialCharacters;
-  }
-
-  //Computer to randomly select characters based on user choices
-  for (var i = 0; i < enter; i++) {
-    var computerChoice = password[Math.floor(Math.random() * password.length)];
-    password.push(computerChoice);
-  }
-
-  //converts password to a string and return password
-  var password = password.join("");
-  UserInput(password);
-  return password;
+  //use slice method to return a password that is set to user's length input
+  return password.slice(0, length);
+}
+//create function to get random value from character array
+function getRandomValue(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

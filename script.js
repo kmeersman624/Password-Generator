9ab7306399243d86fc61092f396109c9d92c9cc0
kmeersman <katie4682@yahoo.com>
2020-05-06 21:43:40 -0600

    Formatted .js with Prettier

--- script.js
+++ script.js
@@ -92,117 +92,133 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
   passwordText.value = password;
 
-function generatePassword() {
-  //Prompt user for pasword
-  var length = prompt("How long would you like password to be?");
+  function generatePassword() {
+    //Prompt user for pasword
+    var length = prompt("How long would you like password to be?");
 
-  length = parseInt(length);
+    length = parseInt(length);
 
-//If not a number, tell user
-  if (isNaN(length)) {
-    alert("That is not a valid number, please try again");
-    return;
-    ("Not a valid number");
+    //If not a number, tell user
+    if (isNaN(length)) {
+      alert("That is not a valid number, please try again");
+      return;
+      ("Not a valid number");
+    }
+    //If not long enough, tell user
+    if (length < 8) {
+      alert("Your password is too short");
+      return;
+    }
+    //If too long, tell user
+    if (length > 128) {
+      alert("Your password is too long");
+      return;
+    }
+    //Check criteria that user wants to include in password.  Must include at least 1 of the criteria
+    var specialChars = confirm(
+      "Click OK to confirm including special characters"
+    );
+    var numericChars = confirm(
+      "Click OK to confirm including numeric characters"
+    );
+    var lowercaseChars = confirm(
+      "Click OK to confirm including lowercase characters"
+    );
+    var uppercaseChars = confirm(
+      "Click OK to confirm including uppercase characters"
+    );
+    //If user picks no elements
+    if (!specialChars && !numericChars && !lowercaseChars && !uppercaseChars) {
+      alert("You need to select at least one!");
+      return;
+    }
   }
-//If not long enough, tell user
-  if (length < 8) {
-    alert("Your password is too short");
-    return;
-  }
-//If too long, tell user
-  if (length > 128) {
-    alert("Your password is too long");
-    return;
-  }
-//Check criteria that user wants to include in password.  Must include at least 1 of the criteria
-  var specialChars = confirm(
-    "Click OK to confirm including special characters"
-  );
-  var numericChars = confirm(
-    "Click OK to confirm including numeric characters"
-  );
-  var lowercaseChars = confirm(
-    "Click OK to confirm including lowercase characters"
-  );
-  var uppercaseChars = confirm(
-    "Click OK to confirm including uppercase characters"
-  );
-//If user picks no elements
-  if (!specialChars && !numericChars && !lowercaseChars && !uppercaseChars) {
-    alert("You need to select at least one!");
-    return;
-  }
-}
-//If user pick special, numeric, and lower case
+  //If user pick special, numeric, and lower case
   if (specialChars && numericChars && lowercaseChars) {
-    password = character.concat(specialCharacters, numericCharacters, lowerCasedCharacters)
-   }
-//If user pick special, numeric, and upper
-  else if (specialChars && numericChars && uppercaseChars) {
-    password = character.concat(specialCharacters, numericCharacters, upperCasedCharacters)
+    password = character.concat(
+      specialCharacters,
+      numericCharacters,
+      lowerCasedCharacters
+    );
   }
-//If user pick special, lower, and upper
+  //If user pick special, numeric, and upper
+  else if (specialChars && numericChars && uppercaseChars) {
+    password = character.concat(
+      specialCharacters,
+      numericCharacters,
+      upperCasedCharacters
+    );
+  }
+  //If user pick special, lower, and upper
   else if (specialChars && lowercaseChars && uppercaseChars) {
-    password = character.concat(specialCharacters, upperCasedCharacters, lowerCasedCharacters)
-}
-//If user pick lower, numeric, and upper
+    password = character.concat(
+      specialCharacters,
+      upperCasedCharacters,
+      lowerCasedCharacters
+    );
+  }
+  //If user pick lower, numeric, and upper
   else if (uppercaseChars && numericChars && lowercaseChars) {
-    password = character.concat(numericCharacters, upperCasedCharacters, lowerCasedCharacters)
-}
-//If user picks upper and lower
+    password = character.concat(
+      numericCharacters,
+      upperCasedCharacters,
+      lowerCasedCharacters
+    );
+  }
+  //If user picks upper and lower
   else if (uppercaseChars && lowercaseChars) {
-    password = character.concat(upperCasedCharacters, lowerCasedCharacters)
-}
-//if user picks lower and numeric
+    password = character.concat(upperCasedCharacters, lowerCasedCharacters);
+  }
+  //if user picks lower and numeric
   else if (numericChars && lowercaseChars) {
-    password = character.concat(numericCharacters, lowerCasedCharacters)
-}
-//if user picks upper and mumeric
+    password = character.concat(numericCharacters, lowerCasedCharacters);
+  }
+  //if user picks upper and mumeric
   else if (numericChars && uppercaseChars) {
-    password = character.concat(numericCharacters, upperCasedCharacters)
-}
-//If user picks special and upper
+    password = character.concat(numericCharacters, upperCasedCharacters);
+  }
+  //If user picks special and upper
   else if (specialChars && uppercaseChars) {
-   password = character.concat(specialCharacters, upperCasedCharacters)
-}
-//If user picks special and lower
+    password = character.concat(specialCharacters, upperCasedCharacters);
+  }
+  //If user picks special and lower
   else if (specialChars && lowercaseChars) {
-    password = character.concat(specialCharacters, lowerCasedCharacters)
-}
-//If user picks special and numeric
+    password = character.concat(specialCharacters, lowerCasedCharacters);
+  }
+  //If user picks special and numeric
   else if (specialChars && numericChars) {
-    password = character.concat(specialCharacters, numericCharacters)
-}
-//If user picks numeric only
+    password = character.concat(specialCharacters, numericCharacters);
+  }
+  //If user picks numeric only
   else if (numericChars) {
-    password = numericCharacters
-}
-//if user picks upper only
+    password = numericCharacters;
+  }
+  //if user picks upper only
   else if (uppercaseChars) {
-    password = upperCasedCharacters
-}
-//if user picks lower only
+    password = upperCasedCharacters;
+  }
+  //if user picks lower only
   else if (lowercaseChars) {
-    password = lowerCasedCharacters
-}
-//if user picks special only
+    password = lowerCasedCharacters;
+  }
+  //if user picks special only
   else if (specialChars) {
-    password = specialCharacters
-}
+    password = specialCharacters;
+  }
 
-//Computer to randomly select characters based on user choices
-for (var i =0; i < enter; i++) {
-  var computerChoice = password[Math.floor(Math.random() * password.length)];
-  password.push(computerChoice);
-  } 
+  //Computer to randomly select characters based on user choices
+  for (var i = 0; i < enter; i++) {
+    var computerChoice = password[Math.floor(Math.random() * password.length)];
+    password.push(computerChoice);
+  }
 
-//converts password to a string and return password
-var password = password.join("");
-UserInput(password);
-return password;
+  //converts password to a string and return password
+  var password = password.join("");
+  UserInput(password);
+  return password;
 }
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
